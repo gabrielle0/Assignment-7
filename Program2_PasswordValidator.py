b5a651cfad3e54dcc5738c582bc0f789cebf197e
unknown <gabrielleapangilinan@iskolarngbayan.pup.edu.ph>
2021-11-27 20:35:45 +0800

    added input password code

---
+++ Program2_PasswordValidator.py
@@ -0,0 +1,10 @@
+#Steps:
+# Create a program that check if password is valid
+#The password is valid if all criteria are met:
+#a. Greater than 15 letters
+#b. Have at least one capital letter
+#c. Have at least one number
+#d. Have at least one special char (!@#$%^&*()_+ etc)
+
+
+
