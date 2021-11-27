d504328a306f37562d6f2af8a6d20c44325799d8
unknown <gabrielleapangilinan@iskolarngbayan.pup.edu.ph>
2021-11-27 20:04:02 +0800

    fixed errors in get_Vowels function

--- Program1.py
+++ Program1.py
@@ -5,26 +5,25 @@
 
 sentence = input ("Type your sentence:  ")
 
+number_Vowels = 0
+
+
 def get_Words (sentence):
     number_Words  = 1
     for character in sentence:
         if character == " ":
             number_Words = number_Words + 1
     return number_Words
-        
-def get_Vowels (sentence):
-    number_Vowels = 0
+
+def get_Vowels (sentence, number_Vowels):
     for character in sentence:
         if character == "a":
-            number_Vowels = number_Vowels + 1 
+            number_Vowels += 1
     return number_Vowels
 
 
-
-
-
 words = get_Words (sentence)
-vowels = get_Vowels (sentence)
+vowels = get_Vowels (sentence, number_Vowels)
 
 print (f"Number of words:{words}")
 print (f"Number of vowels: {vowels}")
