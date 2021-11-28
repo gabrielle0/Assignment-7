ca261fcdbd953e22fb4e1fb283469816291f1e84
unknown <gabrielleapangilinan@iskolarngbayan.pup.edu.ph>
2021-11-28 10:57:45 +0800

    added function for checking second critria

--- Program2_PasswordValidator.py
+++ Program2_PasswordValidator.py
@@ -6,5 +6,39 @@
 #c. Have at least one number
 #d. Have at least one special char (!@#$%^&*()_+ etc)
 
+password = input("Input your password:  ")
+total_Characters = len(password)
+
+last_char = password[total_Characters-1]
+second_last = password[total_Characters-2]
+
+password.islower()
+casecheck = password.islower()
+
+
+def first_Criteria (total_Characters):
+    if total_Characters > 15:
+        criteria1 = 1
+    else:
+        criteria1 = 0
+        print ("Invalid.")
+        print ("Password must have at least 15 characters!")
+    return criteria1
+
+def second_Criteria (casecheck):
+    if casecheck is True:
+            criteria2 = 0
+            print ("Invalid")
+            print ("Password must have at least one capital letter!")
+    else:
+        casecheck is False
+        criteria2 = 1
+    return  criteria2
+
+    
+first = first_Criteria (total_Characters)
+second = second_Criteria (casecheck)
+
+
 
 
